4a32cee3cd220605e79024077188588570e009a4
Alex Lomas <alexlomas951@gmail.com>
2025-03-22 19:41:20 +0800

    Added grid lines, labels for axes, and a legend

--- solar_system_2d_display.py
+++ solar_system_2d_display.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import math
 import subprocess
 
 # Setup figure
 fig, ax = plt.subplots(figsize=(8, 8))
 ax.set_xlim(-35, 35)
+ax.set_xlabel("x / AU")
 ax.set_ylim(-35, 35)
+ax.set_ylabel("y / AU")
 ax.set_aspect("equal")
 ax.set_facecolor("black")
+ax.grid()
 
 # Plotting the sun at position [0, 0]
 ax.plot(0, 0, color="yellow", marker ="o", markersize=15) 
 
 class Planet:
-    def __init__(self, colour):
+    def __init__(self, name, colour):
+        self.name = name
         self.colour = colour
         # Creating marker for the planet
-        self.marker, = ax.plot([], [], color=colour, marker="o", markersize=5)
+        self.marker, = ax.plot([], [], color=colour, marker="o", markersize=5, label=name)
         # Creating orbital path of planet
         self.orbit_path, = ax.plot([], [], color=colour, linestyle="--", linewidth=0.5)
         # Storing coordinates for orbital path
@@ -26,14 +29,14 @@ class Planet:
 
 # Note: the colours are just ones I've chosen as I believe they vaguely match images online, and
 # are not based on actual scientific facts.
-Mercury = Planet("grey")
-Venus = Planet("khaki")
-Earth = Planet("blue")
-Mars = Planet("red")
-Jupiter = Planet("tan")
-Saturn = Planet("wheat")
-Uranus = Planet("lightblue")
-Neptune = Planet("mediumblue")
+Mercury = Planet("Mercury", "grey")
+Venus = Planet("Venus", "khaki")
+Earth = Planet("Earth", "blue")
+Mars = Planet("Mars", "red")
+Jupiter = Planet("Jupiter", "tan")
+Saturn = Planet("Saturn", "wheat")
+Uranus = Planet("Uranus", "lightblue")
+Neptune = Planet("Neptune", "mediumblue")
 
 planets = [Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune]
 
@@ -45,7 +48,8 @@ def update(frame_num):
     # Reads a line of output from orbital engine.
     line = orbit_sim.stdout.readline().strip()
     if not line:
-        return [planet.marker for planet in planets] # Stop if no more data.
+        # Stop if no more data.
+        return [value for planet in planets for value in [planet.marker, planet.orbit_path]] 
     
     values = line.split()
     for i in range(len(values)):
@@ -65,6 +69,7 @@ def update(frame_num):
 # Note: may need to increase number of frames later to allow animation to last longer.
 ani = animation.FuncAnimation(fig, update, frames=1000, interval=20, blit=True)
 
+plt.legend()
 plt.show()
 
 # Close the subprocess when done
