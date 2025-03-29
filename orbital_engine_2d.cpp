7201952b568956a789542553ccc054a9c21e8593
Alex Lomas <alexlomas951@gmail.com>
2025-03-29 16:14:58 +0800

    Replaced Euler method with velocity verlet, changed data types from float to double to increase accuracy

--- orbital_engine_2d.cpp
+++ orbital_engine_2d.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <cmath>
 
-const float pi = 3.14159265358979323846;
-const float AU = 1.496 * pow(10,11); // Astronomical Unit in metres
-const float G = 6.6743 * pow(10,-11); // Gravitational constant
-const float SunMass = 1.989 * pow(10,30); // Mass of sun in kg
+const double AU = 1.496e11; // Astronomical Unit in metres
+const double G = 6.6743e-11; // Gravitational constant
+const double SunMass = 1.989e30; // Mass of sun in kg
 
 class Planet {
     public:
-        float x; //  Current x-coordinate (m)
-        float y; // Current y-coordinate (m)
-        float semi_major_axis; // (m)
-        float eccentricity; // Eccentricity of orbit
-        float v_x; // Current velocity in x-direction (ms^-1)
-        float v_y; // Current velocity in y-direction (ms^-1)
-        float mass; // (kg)
-        void init(float a, float b, float c) {
+        double x; //  Current x-coordinate (m)
+        double y; // Current y-coordinate (m)
+        double semi_major_axis; // (m)
+        double eccentricity; // Eccentricity of orbit
+        double v_x; // Current velocity in x-direction (ms^-1)
+        double v_y; // Current velocity in y-direction (ms^-1)
+        double mass; // (kg)
+        void init(double a, double b, double c) {
             semi_major_axis = a;
             eccentricity = b;
             mass = c;
@@ -30,18 +29,18 @@ class Planet {
         }
 };
 
-void calc_acceleration(float m, float x, float y, float&a_x, float&a_y) {
+void calc_acceleration(double m, double x, double y, double&a_x, double&a_y) {
     /* Force calculted using Newton's law of universal gravitation, with formula F = G * m1 * m2 / (r^2)
     where m1 and m2 are the masses of the two bodies and r is the distance between them. As the sun
     is fixed at [0, 0], the distance is just sqrt(x^2+y^2). */
-    float dist_squared = (x*x)+(y*y);
-    float dist = sqrt(dist_squared);
+    double dist_squared = (x * x) + (y * y);
+    double dist = sqrt(dist_squared);
 
-    float F = G * m * SunMass / dist_squared;
+    double F = G * m * SunMass / dist_squared;
 
     // Force resolved into x and y directions using ratios. 
-    float F_x = F * x / dist;
-    float F_y = F * y / dist;
+    double F_x = F * x / dist;
+    double F_y = F * y / dist;
 
     /* Acceleration calculated using F = ma. It is negative as planet is being accelerated towards the
     the sun, positive would be accelerating the planet away. */
@@ -53,7 +52,7 @@ int main() {
     Planet planets[8]; // array to store Planet objects.
     int planet_count = 0;
     int TimeStep;
-    float semi_major_axis, eccentricity, mass;
+    double semi_major_axis, eccentricity, mass;
 
     std::cin >> TimeStep;
 
@@ -67,17 +66,18 @@ int main() {
     // it while (true), as t isn't actually used
     for (int t=0; t <= 10000; t++) {
         for (int i=0; i < planet_count; i++) {
-            float a_x, a_y;
+            double a_x, a_y, new_a_x, new_a_y;
+            // Calculating planet's velocities and coordinates using velocity verlet integration.
             // a_x and a_y passed by reference.
             calc_acceleration(planets[i].mass, planets[i].x, planets[i].y, a_x, a_y);
 
-            /* Updating planet's velocities and coordinates using Euler method. Time step is 1 day.
-            Smaller time step would increase accuracy, but makes simulation slower. */
-            planets[i].v_x += a_x * TimeStep; 
-            planets[i].v_y += a_y * TimeStep;
+            planets[i].x += planets[i].v_x * TimeStep + 0.5 * a_x * TimeStep * TimeStep;
+            planets[i].y += planets[i].v_y * TimeStep + 0.5 * a_y * TimeStep * TimeStep;
 
-            planets[i].x += planets[i].v_x * TimeStep;
-            planets[i].y += planets[i].v_y * TimeStep;
+            calc_acceleration(planets[i].mass, planets[i].x, planets[i].y, new_a_x, new_a_y);
+
+            planets[i].v_x += 0.5 * (a_x + new_a_x) * TimeStep;
+            planets[i].v_y += 0.5 * (a_y + new_a_y) * TimeStep;
             
             // Converts coordinates to AU and prints them so Python file can read them.
             std::cout << planets[i].x/AU << " " << planets[i].y/AU;
