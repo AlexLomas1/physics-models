bb0c75eb061b0fc0af5feca5b2f2b1ebfe301083
Alex Lomas <alexlomas951@gmail.com>
2025-04-06 10:32:36 +0800

    Updated dynamics engine to update projectiles' positions and to take projectile data from a display file

--- dynamics_engine.cpp
+++ dynamics_engine.cpp
@@ -42,8 +42,62 @@ void calc_acceleration(double mass, double area, double v_x, double v_y, double 
         a_y = -F_y / mass;
     }
 
-    // POTENTIAL PROBLEM: How do I prevent speed exceeding terminal velocity? Can drag force ever be
-    // enough that the ball moves upwards?
     // Adds acceleration due to gravity.
     a_y = a_y - g;
+}
+
+int main() {
+    Projectile projectiles[5];
+    int projectile_count = 0;
+    double TimeStep, h, u, angle, mass, area, drag_coeff;
+
+    std::cin >> TimeStep;
+
+    while (std::cin >> h >> u >> angle >> mass >> area >> drag_coeff && projectile_count < 2) {
+        projectiles[projectile_count].init(h, u, angle, mass, area, drag_coeff);
+        calc_acceleration(projectiles[projectile_count].mass, projectiles[projectile_count].area,
+            projectiles[projectile_count].v_x, projectiles[projectile_count].v_y,
+            projectiles[projectile_count].drag_coeff, projectiles[projectile_count].a_x, 
+            projectiles[projectile_count].a_y); // Calculating initial acceleration
+        projectile_count ++;
+    }
+
+    int projectiles_ended = 0;
+    while (projectiles_ended < projectile_count) {
+        for (int i=0; i < projectile_count; i++) {
+            // If projectile's motion has already ended (but the others' haven't), output last coordinates
+            if (projectiles[i].y <= 0) {
+                std::cout << projectiles[i].x << " " << 0;
+            }
+            else {
+                // Calculating new acceleration using velocity verlet integration.
+                double new_a_x, new_a_y;
+                projectiles[i].x += projectiles[i].v_x * TimeStep + 0.5 * projectiles[i].a_x * TimeStep * TimeStep;
+                projectiles[i].y += projectiles[i].v_y * TimeStep + 0.5 * projectiles[i].a_y * TimeStep * TimeStep;
+
+                calc_acceleration(projectiles[i].mass, projectiles[i].area, projectiles[i].v_x, projectiles[i].v_y,
+                    projectiles[i].drag_coeff, new_a_x, new_a_y);
+                
+                projectiles[i].v_x += 0.5 * (projectiles[i].a_x + new_a_x) * TimeStep;
+                projectiles[i].v_y += 0.5 * (projectiles[i].a_y + new_a_y) * TimeStep;
+
+                projectiles[i].a_x = new_a_x;
+                projectiles[i].a_y = new_a_y;
+
+                if (projectiles[i].y <= 0) {
+                    std::cout << projectiles[i].x << " " << 0;
+                    projectiles_ended ++;
+                }
+                else {
+                    std::cout << projectiles[i].x << " " << projectiles[i].y;
+                }
+
+            }
+            // Ensures that coordinates are seperated by spaces, but there are no trailing spaces.
+            if (i != projectile_count - 1) {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl; // Ends output line
+    }
 }
