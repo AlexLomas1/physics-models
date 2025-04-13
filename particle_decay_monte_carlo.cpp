35f1d7adebefd215d8075635c1a8033fb38fdd6f
Alex Lomas <alexlomas951@gmail.com>
2025-04-13 17:31:32 +0800

    Corrected probability equation; corrected order of receiving data from display file; changed seed for random num generator to be current time

--- particle_decay_monte_carlo.cpp
+++ particle_decay_monte_carlo.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
 #include <random>
 
 int main() {
     int N; // Number of particles not yet decayed.
     int decayed; // Number of particles decayed in one run.
-    float dt; // Time step.
-    float decay_const;
+    float decay_const; // (s^-1)
+    float dt; // Time step (s).
 
     std::cin >> N;
-    std::cin >> dt;
     std::cin >> decay_const;
+    std::cin >> dt;
 
-    float decay_prob = decay_const * dt;
+    // Calculates probability of a particle decaying in a time step as P = 1 - e^(-λΔt)
+    float decay_prob = 1 - exp(-decay_const * dt);
 
-    std::random_device rd; // Generates a seed for the (pseudo-)random number generator.
-    std::mt19937 gen(rd()); // Mersenne Twister generator, uses rd() as a seed.
+    // Mersenne Twister generator, uses current time as a seed.
+    std::mt19937 gen(static_cast<unsigned int>(std::time(nullptr))); 
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
+    // Repeats until all particles have decayed.
     while (N > 0) {
         decayed = 0;
         for (int i=0; i < N; i++) {
-            // Generates random number from 0 to 1.
+            // Generates random number from 0 to 1, normally distributed.
             double r = dis(gen);
             if (r < decay_prob) {
                 decayed +=1;
